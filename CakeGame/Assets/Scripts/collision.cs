6eecbfb7a04a11d228f3f8eb8344930f821d925b
Onni Heinonen <onni.heinonen98@gmail.com>
2019-10-07 12:35:06 +0300

    tower mechanic test

--- CakeGame/Assets/Scripts/collision.cs
+++ CakeGame/Assets/Scripts/collision.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class collision : MonoBehaviour
 {
+    private bool cakeBalance = true;
+    private GameObject cakeBody;
+
+    public GameObject lastCake;
+
+    private void Start()
+    {
+        cakeBody = this.transform.parent.gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("bounce " + collision.name);
-        this.transform.parent.GetComponentInChildren<Animator>().SetTrigger("bounce");
+        if (cakeBalance)
+        {
+            //plays the bounce animation
+            this.transform.parent.GetComponentInChildren<Animator>().SetTrigger("bounce");
+
+            cakeBody.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+
+            if(lastCake != null)
+            {
+                cakeBody.AddComponent<FixedJoint2D>();
+                cakeBody.GetComponent<FixedJoint2D>().connectedBody = lastCake.GetComponent<Rigidbody2D>();
+            }
+        }
+        else
+        {
+            //cake falls
+        }
     }
 }
---
+++ CakeGame/Assets/towerTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerTest : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButton("test"))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, 1));
+        }
+    }
+}
