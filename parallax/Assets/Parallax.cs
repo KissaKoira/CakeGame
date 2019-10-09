d8afe12a09c30bb4b4be66099c4bce444a9e9c79
juusohakala <juuso.h.hakala@gmail.com>
2019-10-09 14:45:20 +0300

    new parallax demo

---
+++ parallax/Assets/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float lenght, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.y;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
+    }
+}
