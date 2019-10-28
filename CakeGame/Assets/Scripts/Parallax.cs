0b413dc3d979fa4a655b57fc61e084ae6be72735
Tero Salmela <tero.salmela@reun.eu>
2019-10-28 15:34:04 +0200

    New background prefab, assets and parallax script

---
+++ CakeGame/Assets/Scripts/Parallax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Parallax : MonoBehaviour
+{
+    private float lenght, startposX, startposY;
+    public GameObject cam;
+    public float parallaxEffectX;
+    public float parallaxEffectY;
+    private float p;
+    private float p2;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        startposX = transform.position.x;
+        startposY = transform.position.y;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //float dist = (cam.transform.position.y * parallaxEffect);
+ 
+        //transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
+ 
+        if (Input.GetButton("test"))
+        {
+            p2 = p + 1;
+           
+        }
+ 
+        p = p + (p2 - p) * 0.05f;
+ 
+        //transform.position = new Vector3(transform.position.x, startpos - parallaxEffect*p, transform.position.z);
+        transform.position = new Vector3(startposX - parallaxEffectX * p, startposY - parallaxEffectY * p, transform.position.z);
+ 
+    }
+}
