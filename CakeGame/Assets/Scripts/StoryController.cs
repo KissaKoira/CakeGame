51d72f045295ff39c42fca43dac1404edb791acf
Tero Salmela <tero.salmela@reun.eu>
2019-12-12 20:28:53 +0200

    Implement story scene and add map scene

---
+++ CakeGame/Assets/Scripts/StoryController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryController : MonoBehaviour
+{
+    public DialogueTrigger dialogueObject;
+    void Start()
+    {
+        dialogueObject.TriggerDialogue();
+    }
+}
