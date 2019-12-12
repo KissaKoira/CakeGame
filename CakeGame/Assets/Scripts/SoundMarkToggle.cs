97a38045726751130dbe7f5a13625a41416b0a37
Tero Salmela <tero.salmela@reun.eu>
2019-12-12 22:01:28 +0200

    Add music and sound mute in options

---
+++ CakeGame/Assets/Scripts/SoundMarkToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundMarkToggle : MonoBehaviour
+{
+    void Awake()
+    {
+        if (this.gameObject.name == "Music")
+        {
+            GetComponent<Toggle>().isOn = !FindObjectOfType<AudioManager>().musicMute;
+            GetComponent<Toggle>().onValueChanged.AddListener((val) => { FindObjectOfType<AudioManager>().ToggleMusicMute(); });
+        }
+        if (this.gameObject.name == "Sound")
+        {
+            GetComponent<Toggle>().isOn = !FindObjectOfType<AudioManager>().soundMute;
+            GetComponent<Toggle>().onValueChanged.AddListener((val) => { FindObjectOfType<AudioManager>().ToggleSoundMute(); });
+        }
+    }
+}
